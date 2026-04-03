035f9bc30c15de8ff7b0417f7557a45bc23edbd1
onehoon <onehoon@gmail.com>
2026-04-03 19:07:01 +0900

    chore: update application version to 0.3.0

--- OptiScaler_Installer.py
+++ OptiScaler_Installer.py
@@ -98,7 +98,7 @@ def _iter_env_file_candidates() -> tuple[Path, ...]:
 
 
  # Application Version
-APP_VERSION = "0.2.4"
+APP_VERSION = "0.3.0"
 # Install flow supports up to two detected GPUs. Dual-GPU requires explicit user selection.
 MAX_SUPPORTED_GPU_COUNT = 2
 
